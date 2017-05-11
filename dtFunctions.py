ae6f24502846b617404a709cab1a1f1604e285c5
aelwood <adam.elwood09@imperial.ac.uk>
2017-05-11 14:10:42 +0100

    added feature importance stuff

--- dtFunctions.py
+++ dtFunctions.py
@@ -1,7 +1,53 @@
+from sklearn.ensemble import ExtraTreesClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
 def classifyScore(score,classification=0):
     '''Returns 0 or 1 depending whether the score is above or below classification'''
     if score > classification: return 1
     else: return 0
 
-def featureImportance(df,classifier,output):
+def featureImportance(df,classifier,output,exceptions=[]):
+    '''Classify the variable importance for a given set of trees code is taken
+    from:
+    http://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html\
+            #sphx-glr-auto-examples-ensemble-plot-forest-importances-py
+    '''
+
+    # Build a forest and compute the feature importances
+    forest = ExtraTreesClassifier(n_estimators=250,
+                                  random_state=0)
+
+    # Make the array of features, X, and the classifier, y
+
+    y = df[classifier]
+    X = df.drop(classifier,axis=1)
+    for e in exceptions:
+        X.drop(e,axis=1,inplace=True)        
+
+    forest.fit(X, y)
+    importances = forest.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+                 axis=0)
+    indices = np.argsort(importances)[::-1]
+
+    # Print the feature ranking
+    outTxtFile = open(os.path.join(output,'featureList.txt'),'w')
+    outTxtFile.write("Feature ranking:\n")
+
+    for f in range(X.shape[1]):
+        outTxtFile.write("%d. feature %s (%f)" % (f + 1, X.columns.values.tolist()[indices[f]], importances[indices[f]])+'\n')
+
+    # Plot the feature importances of the forest
+    plt.figure()
+    plt.title("Feature importances")
+    plt.bar(range(X.shape[1]), importances[indices],
+           color="r", yerr=std[indices], align="center")
+    plt.xticks(range(X.shape[1]), indices)
+    plt.xlim([-1, X.shape[1]])
+    plt.savefig(os.path.join(output,'featureImportances.pdf'))
+    plt.close()
+
     pass
+
