45af2d118a798201c708238b3dc01abf6a1b9170
Adam Elwood <adam.elwood@desy.de>
2018-01-10 17:21:16 +0100

    factorised histogram with error bar plotter

--- histFunctions.py
+++ histFunctions.py
@@ -3,15 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def hist1dError(data,weights=[],**kwargs):
+
+    binCentres,hist,err = hist1dErrorInputs(data,weights)
+
+    plt.bar(binCentres,hist,yerr=err,width=binCentres[1]-binCentres[0],**kwargs)
+    plt.errorbar(binCentres,hist,yerr=err,drawstyle='steps-mid-')
+    pass
+
+def hist1dErrorInputs(data,weights=None,**kwargs):
+
     #if there aren't any weights just set them to one
-    if len(weights)==0: weights = np.ones(len(data))
+    if not weights: weights = np.ones(len(data))
     
     #n,bins = np.histogram(data, **kwargs)
     hist,bins = np.histogram(data, weights=weights, **kwargs)
     err2,bins = np.histogram(data, weights=weights*weights, **kwargs)
     err=np.sqrt(err2)
 
-    plt.bar((bins[1:]+bins[:-1])/2,hist,yerr=err,width=bins[1]-bins[0],color='r',alpha=0.8)
-    #plt.errorbar((bins[1:]+bins[:-1])/2,hist,yerr=err,drawstyle='steps-mid-')
-    pass
+
+    return (bins[1:]+bins[:-1])/2,hist,err
 
